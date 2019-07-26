e2bbe406649932e06d1e121c3f635d7868ae60ef
Marco Mazzai <mazzai.marco@gmail.com>
2019-07-26 14:58:05 +0200

    Updates

---
+++ precache-manifest.dc22da32fc7a2e5c5ff9e67a1ecc1656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c207f1b9779697f003ea1f2721a0431",
+    "url": "/reverse-word-counter-react/index.html"
+  },
+  {
+    "revision": "bcb2944353a8ace99520",
+    "url": "/reverse-word-counter-react/static/css/main.38993bcd.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/reverse-word-counter-react/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "bcb2944353a8ace99520",
+    "url": "/reverse-word-counter-react/static/js/main.3a3468fc.chunk.js"
+  },
+  {
+    "revision": "8f934cce29bd33abcbfd",
+    "url": "/reverse-word-counter-react/static/js/runtime~main.9a7bda7f.js"
+  }
+]);
